f94ee9e69accaac6d47301771856f3b418c66c13
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2013-11-19 16:03:12 +0100

    added support for extensioncontrol notification feed

---
+++ app/code/community/TIG/PostNL/Model/ExtensionControl/Feed.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *                  ___________       __            __   
+ *                  \__    ___/____ _/  |_ _____   |  |  
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/       
+ *          ___          __                                   __   
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_ 
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |  
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|  
+ *                  \/                           \/               
+ *                  ________       
+ *                 /  _____/_______   ____   __ __ ______  
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \ 
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/ 
+ *                        \/                       |__|    
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL: 
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@totalinternetgroup.nl for more information.
+ *
+ * @copyright   Copyright (c) 2013 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class TIG_PostNL_Model_ExtensionControl_Feed extends Mage_AdminNotification_Model_Feed
+{
+    const XML_PATH_SEND_STATISTICS = 'postnl/advanced/send_statistics';
+    const XML_PATH_FEED_USE_HTTPS  = 'postnl/advanced/feed_use_https';
+    const XML_PATH_FEED_URL        = 'postnl/advanced/feed_url';
+
+    /**
+     * Retrieve feed url
+     *
+     * @return string
+     */
+    public function getFeedUrl()
+    {
+        if (!is_null($this->_feedUrl)) {
+            return $this->_feedUrl;
+        }
+        
+        $adminStoreId = Mage_Core_Model_App::ADMIN_STORE_ID;
+        
+        $scheme = 'http://';
+        $useHttps = Mage::getStoreConfigFlag(self::XML_PATH_FEED_USE_HTTPS, $adminStoreId);
+        if ($useHttps) {
+            $scheme = 'https://';
+        }
+        
+        $feedUrl = $scheme . Mage::getStoreConfig(self::XML_PATH_FEED_URL, $adminStoreId);
+        
+        $this->setFeedurl($feedUrl);        
+        return $feedUrl;
+    }
+    
+    /**
+     * Set the feed url
+     * 
+     * @return TIG_PostNL_Model_ExtensionControl_Feed
+     */
+    public function setFeedUrl($feedUrl)
+    {
+        $this->_feedUrl = $feedurl;
+        return $this;
+    }
+}
