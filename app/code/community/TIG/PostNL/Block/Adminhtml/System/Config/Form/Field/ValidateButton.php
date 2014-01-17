e71fc6974e5c2b0d7b9f75639213876f97e01ec2
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2014-01-17 13:04:08 +0100

    added validation button to system config

--- app/code/community/TIG/PostNL/Block/Adminhtml/System/Config/Form/Field/ValidateButton.php
+++ app/code/community/TIG/PostNL/Block/Adminhtml/System/Config/Form/Field/ValidateButton.php
@@ -36,12 +36,45 @@
  * @copyright   Copyright (c) 2013 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
  * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  */
-?>
-<?php $_url = $this->getUrl('postnl/adminhtml_extensionControl/activate', array('_secure' => true)); ?>
-<script type="text/javascript">
-    var postnlActivationUrl = '<?php echo $_url; ?>';
-    function activatePostNL() {
-        $('config_edit_form').action = postnlActivationUrl;
-        configForm.submit();
+class TIG_PostNL_Block_Adminhtml_System_Config_Form_Field_ValidateButton
+    extends Mage_Adminhtml_Block_System_Config_Form_Field
+    implements Varien_Data_Form_Element_Renderer_Interface
+{    
+    /**
+     * Gets the element's html. In this case: a button redirecting the user to the extensionControl controller
+     * 
+     * @return string
+     */
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        $this->setElement($element);
+        
+        $url = $this->getUrl('postnl/adminhtml_extensionControl/activate');
+        
+        $html = $this->getLayout()->createBlock('adminhtml/widget_button')
+                     ->setId($element->getHtmlId())
+                     ->setType('button')
+                     ->setClass('scalable postnl-button')
+                     ->setLabel($this->__('Validate account settings'))
+                     ->setOnClick("validateAccountSettings()")
+                     ->toHtml();
+
+        return $html;
     }
-</script>
+    
+    /**
+     * Render the element without a scope label and without the 'use default' checkbox.
+     * 
+     * @return string
+     * 
+     * @see parent::render()
+     */
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {
+        $element->unsScope()
+                ->unsCanUseWebsiteValue()
+                ->unsCanUseDefaultValue();
+                
+        return parent::render($element);
+    }
+}
