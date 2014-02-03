b4f2745f8dbde0a4fc01e11f4923862324da595a
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2014-02-03 16:21:25 +0100

    experimental code

--- app/code/community/TIG/PostNL/Model/AddressValidation/Webservices/Abstract.php
+++ app/code/community/TIG/PostNL/Model/AddressValidation/Webservices/Abstract.php
@@ -1,28 +1,28 @@
 <?php
 /**
- *                  ___________       __            __   
- *                  \__    ___/____ _/  |_ _____   |  |  
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
  *                    |    |  /  _ \\   __\\__  \  |  |
  *                    |    | |  |_| ||  |   / __ \_|  |__
  *                    |____|  \____/ |__|  (____  /|____/
- *                                              \/       
- *          ___          __                                   __   
- *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_ 
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
  *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
- *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |  
- *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|  
- *                  \/                           \/               
- *                  ________       
- *                 /  _____/_______   ____   __ __ ______  
- *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \ 
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
  *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
- *                 \______  /|__|    \____/ |____/ |   __/ 
- *                        \/                       |__|    
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Creative Commons License.
- * It is available through the world-wide-web at this URL: 
+ * It is available through the world-wide-web at this URL:
  * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  * If you are unable to obtain it through the world-wide-web, please send an email
  * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
@@ -45,35 +45,37 @@ class TIG_PostNL_Model_AddressValidation_Webservices_Abstract extends Varien_Obj
 
     /**
      * Calls a webservice method
-     * 
+     *
      * @param string
      * @param string $method The method that will be called
      * @param array $soapParams An array of parameters to be sent
-     * 
+     *
      * @return object
-     * 
+     *
      * @throws TIG_PostNL_Exception
      */
     public function call($method, $soapParams)
     {
         try {
             $wsdl = self::WEBSERVICE_WSDL_URL;
-            
+
             /**
              * Array of soap options used when connecting to the webservice
              */
             $soapOptions = array(
-                'soap_version' => SOAP_1_1,
-                'features'     => SOAP_SINGLE_ELEMENT_ARRAYS,
+                // 'soap_version' => SOAP_1_1,
+                // 'features'     => SOAP_SINGLE_ELEMENT_ARRAYS,
+                'trace' => true,
+                'cache_wsdl' => WSDL_CACHE_NONE,
             );
-            
+
             /**
              * try to create a new Zend_Soap_Client instance based on the supplied wsdl. if it fails, try again without using the
              * wsdl cache.
              */
             try {
-                $client  = new Zend_Soap_Client(
-                    $wsdl, 
+                $client  = new SoapClient(
+                    $wsdl,
                     $soapOptions
                 );
             } catch (Exception $e) {
@@ -81,44 +83,50 @@ class TIG_PostNL_Model_AddressValidation_Webservices_Abstract extends Varien_Obj
                  * Disable wsdl cache and try again
                  */
                 $soapOptions['cache_wsdl'] = WSDL_CACHE_NONE;
-                
-                $client  = new Zend_Soap_Client(
-                    $wsdl, 
+
+                $client  = new SoapClient(
+                    $wsdl,
                     $soapOptions
                 );
             }
-            
+            echo '<pre>';
+            var_dump($method, $soapParams);
+            // exit;
             /**
              * Call the SOAP method
              */
-            $response = $client->__call(
-                $method,
-                $soapParams
-            );
-            
+            // $response = $client->__call(
+                // $method,
+                // $soapParams
+            // );
+
+            $response = $client->getAdresxpressPostcode('159767', 'Cendris2014!', '1705LN', 21);
+
+            var_dump($response);exit;
             Mage::helper('postnl/webservices')->logWebserviceCall($client);
             return $response;
-        } catch(SoapFault $e) {
-            /**
-             * Log a possible SoapFault exception.
-             */
-            Mage::helper('postnl/webservices')->logWebserviceException($e);
-            
-            throw $e;
-        }
-        // } catch (SoapFault $e) {
-            // $cifHelper = Mage::helper('postnl/cif');
-//         
+        // } catch(SoapFault $e) {
             // /**
-             // * Get the request and response XML data
+             // * Log a possible SoapFault exception.
              // */
-            // if ($client) {
-                // $requestXML  = $cifHelper->formatXml($client->getLastRequest());
-                // $responseXML = $cifHelper->formatXml($client->getLastResponse());
-            // }
-            // echo '<pre>';var_dump(htmlentities($requestXML));
-            // var_dump(htmlentities($responseXML));
-            // exit;
+            // Mage::helper('postnl/webservices')->logWebserviceException($e);
+//
+            // throw $e;
         // }
+        } catch (Exception $e) {
+            $cifHelper = Mage::helper('postnl/cif');
+
+            /**
+             * Get the request and response XML data
+             */
+            if ($client) {
+                $requestXML  = $cifHelper->formatXml($client->__getLastRequest());
+                $responseXML = $cifHelper->formatXml($client->__getLastResponse());
+            }
+
+            echo '<pre>';var_dump(htmlentities($requestXML));
+            var_dump(htmlentities($responseXML));
+            exit;
+        }
     }
 }
--- app/code/community/TIG/PostNL/controllers/AddressValidationController.php
+++ app/code/community/TIG/PostNL/controllers/AddressValidationController.php
@@ -1,28 +1,28 @@
 <?php
 /**
- *                  ___________       __            __   
- *                  \__    ___/____ _/  |_ _____   |  |  
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
  *                    |    |  /  _ \\   __\\__  \  |  |
  *                    |    | |  |_| ||  |   / __ \_|  |__
  *                    |____|  \____/ |__|  (____  /|____/
- *                                              \/       
- *          ___          __                                   __   
- *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_ 
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
  *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
- *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |  
- *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|  
- *                  \/                           \/               
- *                  ________       
- *                 /  _____/_______   ____   __ __ ______  
- *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \ 
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
  *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
- *                 \______  /|__|    \____/ |____/ |   __/ 
- *                        \/                       |__|    
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Creative Commons License.
- * It is available through the world-wide-web at this URL: 
+ * It is available through the world-wide-web at this URL:
  * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  * If you are unable to obtain it through the world-wide-web, please send an email
  * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
@@ -40,9 +40,9 @@ class TIG_PostNL_AddressValidationController extends Mage_Core_Controller_Front_
 {
     /**
      * Validates and enriches a postcode/housenumber combination. This will result in the address's city and streetname if valid.
-     * 
+     *
      * @return TIG_PostNL_AddressValidationController
-     * 
+     *
      * @todo add check to see if response is valid
      */
     public function postcodeCheckAction()
@@ -50,53 +50,57 @@ class TIG_PostNL_AddressValidationController extends Mage_Core_Controller_Front_
         /**
          * Get the address data from the $_POST superglobal
          */
-        $address = $this->getRequest()->getPost();
-        if (!$address
-            || !isset($address['postcode'])
-            || !isset($address['housenumber'])
-        ) {
-            $this->getResponse()
-                 ->setBody('missing data');
-                 
-            return $this;
-        }
-        
-        $postcode = $address['postcode'];
-        $housenumber = $address['housenumber'];
-        
+        // $data = $this->getRequest()->getPost();
+        // if (!$data
+            // || !isset($data['postcode'])
+            // || !isset($data['housenumber'])
+        // ) {
+            // $this->getResponse()
+                 // ->setBody('missing data');
+//
+            // return $this;
+        // }
+//
+        // $postcode = $data['postcode'];
+        // $housenumber = $data['housenumber'];
+
+        $postcode ='1055GH';
+        $housenumber = '8';
+
         /**
          * Load the Cendris webservice and perform an getAdresxpressPostcode request
          */
         $webservice = Mage::getModel('postnl_addressvalidation/webservices');
-        
+
         try {
             $result = $webservice->getAdresxpressPostcode($postcode, $housenumber);
         } catch (Exception $e) {
+            echo $e->getMessage();exit;
             Mage::helper('postnl')->logException($e);
-            
+
             $this->getResponse()
                  ->setBody('error');
-            
+
             return $this;
         }
-        
+
         /**
          * @todo add check to see if result is valid
          */
-        
+
         /**
          * Get the city and streetname from the response
          */
         $city = $result->woonplaats;
         $streetname = $result->straatnaam;
-        
+
         /**
          * If either the city or streetname is empty we have an invalid response
          */
         if (empty($city) || empty($streetname)) {
             $this->getResponse()
                  ->setBody('error');
-            
+
             return $this;
         }
 
@@ -107,16 +111,16 @@ class TIG_PostNL_AddressValidationController extends Mage_Core_Controller_Front_
             'city'       => $city,
             'streetname' => $streetname,
         );
-        
+
         $response = Mage::helper('core')->jsonEncode($responseArray);
-        
+
         /**
          * Return the result as a json response
          */
         $this->getResponse()
              ->setHeader('Content-type', 'application/x-json')
              ->setBody($response);
-             
+
         return $this;
     }
 }
