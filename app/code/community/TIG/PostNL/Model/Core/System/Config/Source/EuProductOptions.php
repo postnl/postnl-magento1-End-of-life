b01cc4c6d403412bbc9d4e882421a449e848799b
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2014-02-04 09:14:21 +0100

    EPS to BE option is now deprecated

--- app/code/community/TIG/PostNL/Model/Core/System/Config/Source/EuProductOptions.php
+++ app/code/community/TIG/PostNL/Model/Core/System/Config/Source/EuProductOptions.php
@@ -1,28 +1,28 @@
 <?php
 /**
- *                  ___________       __            __   
- *                  \__    ___/____ _/  |_ _____   |  |  
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
  *                    |    |  /  _ \\   __\\__  \  |  |
  *                    |    | |  |_| ||  |   / __ \_|  |__
  *                    |____|  \____/ |__|  (____  /|____/
- *                                              \/       
- *          ___          __                                   __   
- *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_ 
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
  *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
- *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |  
- *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|  
- *                  \/                           \/               
- *                  ________       
- *                 /  _____/_______   ____   __ __ ______  
- *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \ 
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
  *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
- *                 \______  /|__|    \____/ |____/ |   __/ 
- *                        \/                       |__|    
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Creative Commons License.
- * It is available through the world-wide-web at this URL: 
+ * It is available through the world-wide-web at this URL:
  * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  * If you are unable to obtain it through the world-wide-web, please send an email
  * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
@@ -42,12 +42,12 @@ class TIG_PostNL_Model_Core_System_Config_Source_EuProductOptions
      * XML path to supported options configuration setting
      */
     const XML_PATH_SUPPORTED_PRODUCT_OPTIONS = 'postnl/cif_product_options/supported_product_options';
-    
+
     /**
      * Returns an option array for all possible PostNL product options
-     * 
+     *
      * @return array
-     * 
+     *
      * @todo implement COD and extra cover
      */
     public function toOptionArray()
@@ -58,6 +58,11 @@ class TIG_PostNL_Model_Core_System_Config_Source_EuProductOptions
                 'value' => '4952',
                 'label' => $helper->__('EU Pack Special Consumer (incl. signature)'),
             ),
+            /**
+             * This option will be removed in v1.2.0
+             *
+             * @deprecated v1.1.2
+             */
             array(
                 'value' => '4955',
                 'label' => $helper->__('EU Pack Standard (Belgium only, no signature)'),
@@ -65,8 +70,8 @@ class TIG_PostNL_Model_Core_System_Config_Source_EuProductOptions
             ),
             /**
              * These are not currently implemented
-             * 
-             * TODO implement these options
+             *
+             * @todo implement these options
              */
             /*
             array(
@@ -78,16 +83,16 @@ class TIG_PostNL_Model_Core_System_Config_Source_EuProductOptions
                 'label' => $helper->__('EU Pack Special COD (Belgium and Luxembourg only)'),
             ),*/
         );
-        
+
         return $availableOptions;
     }
-    
+
     /**
      * Get a list of available options. This is a filtered/modified version of the array supplied by toOptionArray();
-     * 
+     *
      * @param boolean|int $storeId
      * @param boolean $codesOnly
-     * 
+     *
      * @return array
      */
     public function getAvailableOptions($storeId = false, $codesOnly = false)
@@ -95,21 +100,21 @@ class TIG_PostNL_Model_Core_System_Config_Source_EuProductOptions
         if ($storeId === false) {
             $storeId = Mage_Core_Model_App::ADMIN_STORE_ID;
         }
-        
+
         $helper = Mage::helper('postnl');
         $options = $this->toOptionArray();
-        
+
         /**
          * Get a list of all possible options
          */
         $availableOptions = array();
-        
+
         /**
          * Get the list of supported product options from the shop's configuration
          */
         $supportedOptions = Mage::getStoreConfig(self::XML_PATH_SUPPORTED_PRODUCT_OPTIONS, $storeId);
         $supportedOptionsArray = explode(',', $supportedOptions);
-        
+
         /**
          * Check each standard option to see if it's supprted
          */
@@ -118,15 +123,15 @@ class TIG_PostNL_Model_Core_System_Config_Source_EuProductOptions
             if (!in_array($option['value'], $supportedOptionsArray)) {
                 continue;
             }
-            
+
             if ($codesOnly === true) {
                 $availableOptions[] = $option['value'];
                 continue;
             }
-            
+
             $availableOptions[] = $option;
         }
-        
+
         return $availableOptions;
     }
 }
