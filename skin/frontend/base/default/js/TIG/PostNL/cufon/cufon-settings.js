f6e5732b578a604fcf110d3f405f9082dc05ba78
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2014-03-13 10:56:29 +0100

    more changes by mark

--- skin/frontend/base/default/js/TIG/PostNL/cufon/cufon-settings.js
+++ skin/frontend/base/default/js/TIG/PostNL/cufon/cufon-settings.js
@@ -1,4 +1,4 @@
 initCufon = function() {
     Cufon.replace('.postnl-container', { fontFamily: 'PostNL Light' });
-    Cufon.replace('.postnl-container strong, .postnl-container h1, .postnl-container h2, .postnl-container h3', { fontFamily: 'PostNL Medium' });
+    Cufon.replace('.postnl-container strong, .postnl-container h1, .postnl-container h2, .postnl-container h3, .postnl-container .button span', { fontFamily: 'PostNL Medium' });
 };
