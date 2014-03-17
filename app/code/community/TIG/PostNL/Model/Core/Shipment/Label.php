6c53823db9a3e1d6a4fb280af5ffc1dd9decd213
TIG - development <sysops@totalinternetgroup.nl>
2014-03-17 12:17:44 +0100

    various code improvements

--- app/code/community/TIG/PostNL/Model/Core/Shipment/Label.php
+++ app/code/community/TIG/PostNL/Model/Core/Shipment/Label.php
@@ -35,9 +35,7 @@
  *
  * @copyright   Copyright (c) 2013 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
  * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
- */
-
-/**
+ *
  * Class TIG_PostNL_Model_Core_Shipment_Label
  *
  * @method string getLabelType()
