708a7ef054b7c6a6415974f6032b4192b2062e9d
TIG - development <sysops@totalinternetgroup.nl>
2014-04-03 16:10:59 +0200

    added option to use PostNL-specific tablerates

--- app/code/community/TIG/PostNL/sql/postnl_setup/upgrade-1.1.4-1.2.0.php
+++ app/code/community/TIG/PostNL/sql/postnl_setup/upgrade-1.1.4-1.2.0.php
@@ -144,4 +144,76 @@ $conn->addColumn($installer->getTable('postnl_checkout/order'),
     )
 );
 
+/***********************************************************************************************************************
+ * POSTNL TABLERATE
+ **********************************************************************************************************************/
+
+$table = $installer->getConnection()
+                   ->newTable($installer->getTable('postnl_carrier/tablerate'));
+$table->addColumn('pk', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity'  => true,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ), 'Primary key')
+    ->addColumn('website_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Website Id')
+    ->addColumn('dest_country_id', Varien_Db_Ddl_Table::TYPE_TEXT, 4, array(
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Destination coutry ISO/2 or ISO/3 code')
+    ->addColumn('dest_region_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'nullable'  => false,
+        'default'   => '0',
+    ), 'Destination Region Id')
+    ->addColumn('dest_zip', Varien_Db_Ddl_Table::TYPE_TEXT, 10, array(
+        'nullable'  => false,
+        'default'   => '*',
+    ), 'Destination Post Code (Zip)')
+    ->addColumn('condition_name', Varien_Db_Ddl_Table::TYPE_TEXT, 20, array(
+        'nullable'  => false,
+    ), 'Rate Condition name')
+    ->addColumn('condition_value', Varien_Db_Ddl_Table::TYPE_DECIMAL, '12,4', array(
+        'nullable'  => false,
+        'default'   => '0.0000',
+    ), 'Rate condition value')
+    ->addColumn('price', Varien_Db_Ddl_Table::TYPE_DECIMAL, '12,4', array(
+        'nullable'  => false,
+        'default'   => '0.0000',
+    ), 'Price')
+    ->addColumn('cost', Varien_Db_Ddl_Table::TYPE_DECIMAL, '12,4', array(
+        'nullable'  => false,
+        'default'   => '0.0000',
+    ), 'Cost')
+    ->addIndex(
+        $installer->getIdxName(
+            'postnl_carrier/tablerate',
+            array(
+                'website_id',
+                'dest_country_id',
+                'dest_region_id',
+                'dest_zip',
+                'condition_name',
+                'condition_value'
+            ),
+            Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
+        ),
+        array(
+            'website_id',
+            'dest_country_id',
+            'dest_region_id',
+            'dest_zip',
+            'condition_name',
+            'condition_value'
+        ),
+        array(
+            'type' => Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
+        )
+    )
+    ->setComment('PostNL Tablerate');
+
+$installer->getConnection()->createTable($table);
+
 $installer->endSetup();
