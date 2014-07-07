08969290c0773c53c91f01393e28a8d2d1629201
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2014-07-07 11:58:32 +0200

    added support for sales email templates

--- app/code/community/TIG/PostNL/Model/Payment/Cod.php
+++ app/code/community/TIG/PostNL/Model/Payment/Cod.php
@@ -184,11 +184,76 @@ class TIG_PostNL_Model_Payment_Cod extends Mage_Payment_Model_Method_Abstract
         $parentIsAvailable = parent::isAvailable($quote);
         if (!$parentIsAvailable) {
             $helper->log(
-                $helper->__("PostNL COD is not available, because the abstract isAvailable() check returned 'false'")
+                $helper->__("PostNL COD is not available, because the base isAvailable() check returned 'false'")
             );
         }
 
         return $parentIsAvailable;
+    }
+
+    /**
+     * Check whether payment method is applicable to quote.
+     * Purposed to allow use in controllers some logic that was implemented in blocks only before.
+     *
+     * Overloaded to expand the CHECK_USE_FOR_COUNTRY check with the shipping address.
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     * @param int|null               $checksBitMask
+     *
+     * @return bool
+     */
+    public function isApplicableToQuote($quote, $checksBitMask)
+    {
+        if ($checksBitMask & self::CHECK_USE_FOR_COUNTRY) {
+            if (!$this->canUseForCountry($quote->getBillingAddress()->getCountry())) {
+                return false;
+            }
+            if (!$this->canUseForCountry($quote->getShippingAddress()->getCountry())) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_USE_FOR_CURRENCY) {
+            if (!$this->canUseForCurrency($quote->getStore()->getBaseCurrencyCode())) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_USE_CHECKOUT) {
+            if (!$this->canUseCheckout()) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_USE_FOR_MULTISHIPPING) {
+            if (!$this->canUseForMultishipping()) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_USE_INTERNAL) {
+            if (!$this->canUseInternal()) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_ORDER_TOTAL_MIN_MAX) {
+            $total = $quote->getBaseGrandTotal();
+            $minTotal = $this->getConfigData('min_order_total');
+            $maxTotal = $this->getConfigData('max_order_total');
+            if (!empty($minTotal) && $total < $minTotal || !empty($maxTotal) && $total > $maxTotal) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_RECURRING_PROFILES) {
+            if (!$this->canManageRecurringProfiles() && $quote->hasRecurringItems()) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_ZERO_TOTAL) {
+            $total = $quote->getBaseSubtotal() + $quote->getShippingAddress()->getBaseShippingAmount();
+            if ($total < 0.0001 && $this->getCode() != 'free'
+                && !($this->canManageRecurringProfiles() && $quote->hasRecurringItems())
+            ) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
