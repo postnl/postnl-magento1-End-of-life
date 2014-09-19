7001605dbfb6162f66c634ee1b67375f71c4f684
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2014-09-19 17:02:01 +0200

    modified all instances of totalinternetgroup.nl to tig.nl

--- app/code/community/TIG/PostNL/Model/Payment/Service.php
+++ app/code/community/TIG/PostNL/Model/Payment/Service.php
@@ -25,15 +25,15 @@
  * It is available through the world-wide-web at this URL:
  * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  * If you are unable to obtain it through the world-wide-web, please send an email
- * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ * to servicedesk@tig.nl so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade this module to newer
  * versions in the future. If you wish to customize this module for your
- * needs please contact servicedesk@totalinternetgroup.nl for more information.
+ * needs please contact servicedesk@tig.nl for more information.
  *
- * @copyright   Copyright (c) 2014 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @copyright   Copyright (c) 2014 Total Internet Group B.V. (http://www.tig.nl)
  * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
  */
 class TIG_PostNL_Model_Payment_Service
@@ -44,7 +44,7 @@ class TIG_PostNL_Model_Payment_Service
      * This is a really annoying hack to fix the problem where the full tax info does not include the custom PostNL COD
      * fee tax info. Magento only supports tax info from shipping tax or product tax by default
      * (see Mage_Tax_Helper_Data::getCalculatedTaxes()). If anybody knows of a better way to fix this (that does not
-     * require a core rewrite) please let us know at servicedesk@totalinternetgroup.nl.
+     * require a core rewrite) please let us know at servicedesk@tig.nl.
      *
      * @param array                                                                                   $fullInfo
      * @param Mage_Sales_Model_Order|Mage_Sales_Model_Order_Invoice|Mage_Sales_Model_Order_Creditmemo $source
