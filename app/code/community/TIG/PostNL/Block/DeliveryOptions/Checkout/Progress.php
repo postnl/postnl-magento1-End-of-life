1f4ceebf86c8cdd68952c2e7e3e5416520f6ae05
Joris Fritzsche <joris.fritzsche@tig.nl>
2014-12-01 14:12:30 +0100

    added pakje gemak address to checkout progress block

---
+++ app/code/community/TIG/PostNL/Block/DeliveryOptions/Checkout/Progress.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) 2014 Total Internet Group B.V. (http://www.tig.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+class TIG_PostNL_Block_DeliveryOptions_Checkout_Progress extends TIG_PostNL_Block_DeliveryOptions_Template
+{
+    /**
+     * @var string
+     */
+    protected $_eventPrefix = 'postnl_deliveryoptions_checkout_progress';
+
+    /**
+     * @var Mage_Checkout_Model_Session
+     */
+    protected $_checkout;
+
+    /**
+     * Retrieve checkout session model
+     *
+     * @return Mage_Checkout_Model_Session
+     */
+    public function getCheckout()
+    {
+        if (empty($this->_checkout)) {
+            $this->_checkout = Mage::getSingleton('checkout/session');
+        }
+        return $this->_checkout;
+    }
+
+    /**
+     * @return string|false
+     */
+    public function getPakjeGemakAddress()
+    {
+        $quote =$this->getCheckout()->getQuote();
+
+        $pakjeGemakAddress = false;
+        /** @var Mage_Sales_Model_Quote_Address $address */
+        foreach ($quote->getAllAddresses() as $address) {
+            if ($address->getAddressType() == 'pakje_gemak') {
+                $pakjeGemakAddress = $address;
+                break;
+            }
+        }
+
+        if (!$pakjeGemakAddress) {
+            return false;
+        }
+
+        return $pakjeGemakAddress->format('html');
+    }
+}
