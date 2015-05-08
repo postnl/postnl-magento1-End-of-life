ee6441af2b433c3dc4c2bdd2f788cc8d49c96c72
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2015-05-08 10:13:17 +0200

    fixed bug

--- app/code/community/TIG/PostNL/Model/DeliveryOptions/Observer/ShippingMethodAvailable.php
+++ app/code/community/TIG/PostNL/Model/DeliveryOptions/Observer/ShippingMethodAvailable.php
@@ -260,33 +260,34 @@ class TIG_PostNL_Model_DeliveryOptions_Observer_ShippingMethodAvailable extends 
         /**
          * @var TIG_PostNL_Block_DeliveryOptions_Checkout_DeliveryOptions $firstChild
          */
-        $firstChild = $block->getLayout()->createBlock(
+        $deliveryOptionsBlock = $block->getLayout()->createBlock(
             'postnl_deliveryoptions/checkout_deliveryOptions',
             'postnl.osc.delivery.options'
         );
-        $firstChild->setTemplate('TIG/PostNL/delivery_options/onestepcheckout/deliveryoptions.phtml');
+        $deliveryOptionsBlock->setTemplate('TIG/PostNL/delivery_options/onestepcheckout/deliveryoptions.phtml');
 
         /**
-         * @var Mage_Core_Block_Template $secondChild
+         * @var Mage_Core_Block_Template $addLocationBlock
          */
-        $secondChild = $block->getLayout()->createBlock(
+        $addLocationBlock = $block->getLayout()->createBlock(
             'core/template',
             'postnl.osc.add.location'
         );
-        $secondChild->setTemplate('TIG/PostNL/delivery_options/addlocation.phtml');
+        $addLocationBlock->setTemplate('TIG/PostNL/delivery_options/addlocation.phtml');
 
         /**
-         * @var TIG_PostNL_Block_DeliveryOptions_Checkout_AddPhoneNumber $thirdChild
+         * @var TIG_PostNL_Block_DeliveryOptions_Checkout_AddPhoneNumber $addPhoneNumberBlock
          */
-        $thirdChild = $block->getLayout()->createBlock(
+        $addPhoneNumberBlock = $block->getLayout()->createBlock(
             'postnl_deliveryoptions/checkout_addPhoneNumber',
             'postnl.add.phonenumber'
         );
-        $thirdChild->setTemplate('TIG/PostNL/delivery_options/addphonenumber.phtml');
+        $addPhoneNumberBlock->setTemplate('TIG/PostNL/delivery_options/addphonenumber.phtml');
 
-        $secondChild->append($thirdChild);
-        $firstChild->append($secondChild);
-        $block->append($firstChild);
+        $deliveryOptionsBlock->append($addLocationBlock)
+                             ->append($addPhoneNumberBlock);
+
+        $block->append($deliveryOptionsBlock);
 
         return $block;
     }
@@ -302,35 +303,36 @@ class TIG_PostNL_Model_DeliveryOptions_Observer_ShippingMethodAvailable extends 
     protected function _addGoMageDeliveryOptionBlocks(Mage_Checkout_Block_Onepage_Shipping_Method_Available $block)
     {
         /**
-         * @var TIG_PostNL_Block_DeliveryOptions_Checkout_GoMage_LightCheckout_DeliveryOptions $firstChild
+         * @var TIG_PostNL_Block_DeliveryOptions_Checkout_GoMage_LightCheckout_DeliveryOptions $deliveryOptionsBlock
          */
-        $firstChild = $block->getLayout()->createBlock(
+        $deliveryOptionsBlock = $block->getLayout()->createBlock(
             'postnl_deliveryoptions/checkout_goMage_lightCheckout_deliveryOptions',
             'postnl.gomage.delivery.options'
         );
-        $firstChild->setTemplate('TIG/PostNL/delivery_options/gomage_checkout/deliveryoptions.phtml');
+        $deliveryOptionsBlock->setTemplate('TIG/PostNL/delivery_options/gomage_checkout/deliveryoptions.phtml');
 
         /**
-         * @var Mage_Core_Block_Template $secondChild
+         * @var Mage_Core_Block_Template $addLocationBlock
          */
-        $secondChild = $block->getLayout()->createBlock(
+        $addLocationBlock = $block->getLayout()->createBlock(
             'core/template',
             'postnl.gomage.add.location'
         );
-        $secondChild->setTemplate('TIG/PostNL/delivery_options/addlocation.phtml');
+        $addLocationBlock->setTemplate('TIG/PostNL/delivery_options/addlocation.phtml');
 
         /**
-         * @var TIG_PostNL_Block_DeliveryOptions_Checkout_AddPhoneNumber $thirdChild
+         * @var TIG_PostNL_Block_DeliveryOptions_Checkout_AddPhoneNumber $addPhoneNumberBlock
          */
-        $thirdChild = $block->getLayout()->createBlock(
+        $addPhoneNumberBlock = $block->getLayout()->createBlock(
             'postnl_deliveryoptions/checkout_addPhoneNumber',
             'postnl.add.phonenumber'
         );
-        $thirdChild->setTemplate('TIG/PostNL/delivery_options/addphonenumber.phtml');
+        $addPhoneNumberBlock->setTemplate('TIG/PostNL/delivery_options/addphonenumber.phtml');
 
-        $firstChild->append($secondChild)
-                   ->append($thirdChild);
-        $block->append($firstChild);
+        $deliveryOptionsBlock->append($addLocationBlock)
+                             ->append($addPhoneNumberBlock);
+
+        $block->append($deliveryOptionsBlock);
 
         return $block;
     }
