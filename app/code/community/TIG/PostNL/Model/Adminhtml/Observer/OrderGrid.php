e0437b14a62e0311df623ed13b2753a7e45a3c69
Tom Groskamp <tom.groskamp@tig.nl>
2015-12-28 11:59:46 +0100

    fixed a bug where standard product options are shown for PG orders in the massaction select box

--- app/code/community/TIG/PostNL/Model/Adminhtml/Observer/OrderGrid.php
+++ app/code/community/TIG/PostNL/Model/Adminhtml/Observer/OrderGrid.php
@@ -1091,9 +1091,8 @@ class TIG_PostNL_Model_Adminhtml_Observer_OrderGrid extends Varien_Object
             ),
             'postnl_pg_options' => $optionsModel->getOptions(
                 array(
-                    'group'   => 'standard_options',
+                    'group'   => 'pakjegemak_options',
                     'isCod'   => false,
-                    'isAvond' => true,
                 ),
                 false,
                 true
