4d2aa276997b1d8aaab714051605c9f666f62493
Joris Fritzsche <joris.fritzsche@totalinternetgroup.nl>
2016-02-26 11:53:30 +0100

    disabled return labels for food shipments

--- app/code/community/TIG/PostNL/Model/Core/Shipment.php
+++ app/code/community/TIG/PostNL/Model/Core/Shipment.php
@@ -3160,7 +3160,7 @@ class TIG_PostNL_Model_Core_Shipment extends Mage_Core_Model_Abstract
      */
     public function canPrintReturnLabels()
     {
-        if (!$this->isDomesticShipment() || $this->isBuspakjeShipment()) {
+        if (!$this->isDomesticShipment() || $this->isBuspakjeShipment() || $this->isFoodShipment()) {
             return false;
         }
 
