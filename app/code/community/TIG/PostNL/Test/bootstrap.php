1f16b1e9d3bcccf1e471f71906a52cd694483a00
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2016-03-22 15:44:46 +0100

    Added Functional test

--- app/code/community/TIG/PostNL/Test/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(__DIR__ . '/../../../../../../Mage.php');
-
-ini_set('display_errors', true);
-error_reporting(-1);
-TIG_PostNL_Test_Framework_TIG_Test_TestCase::resetMagento();
