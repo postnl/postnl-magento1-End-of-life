c30852265a4de2d006be932da427c22d0d500265
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2016-06-22 11:08:48 +0200

    Changed curl to wget to download PHPUnit

--- app/code/community/TIG/PostNL/Test/setup.sh
+++ app/code/community/TIG/PostNL/Test/setup.sh
@@ -33,7 +33,7 @@ if [ ! -f "${TOOLS}/modman" ]; then
 fi
 
 if [ ! -f "${TOOLS}/phpunit" ]; then
-    curl https://phar.phpunit.de/phpunit-old.phar -o "${TOOLS}/phpunit"
+    wget https://phar.phpunit.de/phpunit-old.phar -O "${TOOLS}/phpunit"
     chmod +x "${TOOLS}/phpunit"
 fi
 
@@ -68,7 +68,7 @@ cd "${PUBLIC_DIR}/.modman/project";
 mkdir -p "${PUBLIC_DIR}var/session";
 chmod -R 777 "${PUBLIC_DIR}var/session";
 
-"${TOOLS}/phpunit" -c app/code/community/TIG/PostNL/Test/phpunit.xml
+"${TOOLS}/phpunit" -c "${PUBLIC_DIR}/app/code/community/TIG/PostNL/Test/phpunit.xml"
 
 mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}\`;"
 #echo "Deleting ${BUILDENV}"
