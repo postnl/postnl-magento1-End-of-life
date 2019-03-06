57ce3c5c28ae4692c69aca2a1436829668f50def
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2019-03-06 12:40:47 +0100

    POSTNLM1-694 - Removed UK/GB from EPS and updated generateLabel to 2.2

--- app/code/community/TIG/PostNL/Helper/Cif.php
+++ app/code/community/TIG/PostNL/Helper/Cif.php
@@ -101,8 +101,6 @@ class TIG_PostNL_Helper_Cif extends TIG_PostNL_Helper_Data
         'EE',
         'FI',
         'FR',
-        'GB',
-        'UK', //alias for GB
         'HU',
         'IE',
         'IT',
