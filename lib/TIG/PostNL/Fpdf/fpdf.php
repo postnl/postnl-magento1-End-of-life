551df171e25fdb6c3290d4d07491cd178d19bcc0
Kevin van Dijk <kevin.van.dijk@tig.nl>
2021-12-17 11:13:26 +0100

    magic_quotes deprecations

--- lib/TIG/PostNL/Fpdf/fpdf.php
+++ lib/TIG/PostNL/Fpdf/fpdf.php
@@ -1068,8 +1068,13 @@ function _dochecks()
 	if(ini_get('mbstring.func_overload') & 2)
 		$this->Error('mbstring overloading must be disabled');
 	// Ensure runtime magic quotes are disabled
-	if(get_magic_quotes_runtime())
-		@set_magic_quotes_runtime(0);
+	if(function_exists('get_magic_quotes_runtime')) {
+	    if(@get_magic_quotes_runtime()) {
+	        if(function_exists('set_magic_quotes_runtime')) {
+	            @set_magic_quotes_runtime(0);
+            }
+        }
+    }
 }
 
 function _checkoutput()
